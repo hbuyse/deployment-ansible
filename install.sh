2597decf963a9ff7d51ebca45cfc596940a4d75c
Henri Buyse <henri.buyse@gmail.com>
2025-02-11 22:15:36 +0100

    feat(install): create install script

---
+++ install.sh
@@ -0,0 +1,180 @@
+#! /usr/bin/env bash
+
+readonly RED="\e[1;31m"
+readonly GREEN="\e[1;32m"
+readonly YELLOW="\e[1;33m"
+readonly DEFAULT="\e[0m"
+
+readonly OS="${CHEZMOI_OS}"
+readonly ID="${CHEZMOI_OS_RELEASE_ID}"
+readonly IDLIKE="${CHEZMOI_OS_RELEASE_ID_LIKE}"
+
+if [ "${CHEZMOI_UID}" -eq 0 ]; then
+    readonly SUDO=""
+else
+    readonly SUDO="sudo"
+fi
+
+function check_sudo() {
+    if [ -n "${SUDO}" ]; then
+        echo -en "Asking for 'sudo' rights: "
+        sudo -p "" -v && echo -e "${GREEN}OK${DEFAULT}" || echo -e "${RED}KO${DEFAULT}"
+    fi
+}
+
+function prompt() {
+    echo -en "$*: "
+}
+
+function display_already_installed() {
+    echo -e "${YELLOW}already installed${DEFAULT}"
+}
+
+function display_ko_ok() {
+    [ "${1}" -eq 0 ] && echo -e "${GREEN}OK${DEFAULT}" || echo -e "${RED}KO${DEFAULT}"
+}
+
+function cmdexists() {
+    command -v "$1" > /dev/null && return 0 || return 1
+}
+
+function display_info() {
+    # Display some information about the install
+    echo "==================================="
+    echo "Script: ${1}"
+    echo "-----------------------------------"
+    echo "Operating System: ${OS}"
+    echo "Distribution ID: ${ID}"
+    echo "Distribution IDLike: ${IDLIKE}"
+    echo "User: ${USER}"
+    echo "==================================="
+}
+
+# Install packages based on the OS.
+# Check that the package is installed before installing it.
+function aur_install_packages() {
+    local packages_to_install=("$@")
+    local packages_not_installed=()
+    local install_cmd=""
+
+    if [ "${OS}-${ID}" = "linux-arch" ]; then
+        install_packages \
+            git \
+            base-devel
+        git clone https://aur.archlinux.org/yay.git /tmp/yay
+        makepkg --syncdeps --install --needed --noconfirm --nocheck --dir /tmp/yay
+        rm -rf /tmp/yay
+    fi
+
+    # Get the command to install package and check that package is installed
+    case "${OS}-${ID}" in
+    "linux-endeavouros" | "linux-manjaro" | "linux-arch")
+        install_cmd="yay --sync --refresh --refresh --needed --answerclean NotInstalled --answerdiff NotInstalled"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if not already installed
+            if ! yay --query --search --quiet "${pkg}" | grep -qw "${pkg}" 2>&1; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    *)
+        echo "Unsupported distribution '${ID}' (based on OS '${OS}')"
+        return
+        ;;
+    esac
+
+    # Install only the packages that are not already installed
+    if [ ${#packages_not_installed[@]} -ne 0 ]; then
+        check_sudo
+
+        prompt "Installing ${packages_not_installed[*]} using AUR"
+        # shellcheck disable=SC2086
+        ${SUDO} ${install_cmd} "${packages_not_installed[@]}"
+        display_ko_ok $?
+    fi
+}
+
+# Install packages based on the OS.
+# Check that the package is installed before installing it.
+function install_packages() {
+    local packages_to_install=("$@")
+    local packages_not_installed=()
+    local install_cmd=""
+
+    # Get the command to install package and check that package is installed
+    case "${OS}-${ID}" in
+    "linux-endeavouros" | "linux-manjaro" | "linux-arch")
+        install_cmd="pacman --sync --refresh --refresh --sysupgrade --needed --noconfirm"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if not already installed
+            if ! pacman --query --search --quiet "${pkg}" | grep -qw "${pkg}" 2>&1; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    "linux-ubuntu")
+        install_cmd="apt-get install --assume-yes --quiet"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if not already installed
+            if ! dpkg --get-selections | grep -w "${pkg}" | awk '{ print $2 }' | grep -q -w 'install'; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    "freebsd-freebsd")
+        install_cmd="pkg install --automatic --yes"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if already installed
+            if ! pkg info | grep -qw "${pkg}"; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    "linux-opensuse-tumbleweed")
+        install_cmd="zypper --quiet --non-interactive install"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if already installed
+            if ! zypper --quiet search --installed-only --match-exact ${pkg} > /dev/null 2>&1; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    "linux-fedora")
+        install_cmd="dnf --asssumeyes --quiet --skip-unavailables install"
+        # Check that package is installed
+        for pkg in "${packages_to_install[@]}"; do
+            # Check if already installed
+            if ! dnf list --installed "${pkg}" > /dev/null 2>&1; then
+                packages_not_installed+=("${pkg}")
+            fi
+        done
+        ;;
+
+    *)
+        echo "Unsupported distribution '${ID}' (based on OS '${OS}')"
+        return
+        ;;
+    esac
+
+    # Install only the packages that are not already installed
+    if [ ${#packages_not_installed[@]} -ne 0 ]; then
+        check_sudo
+
+        prompt "Installing ${packages_not_installed[*]}"
+        # shellcheck disable=SC2086
+        ${SUDO} ${install_cmd} "${packages_not_installed[@]}"
+        display_ko_ok $?
+    fi
+}
+
+display_info ${0}
