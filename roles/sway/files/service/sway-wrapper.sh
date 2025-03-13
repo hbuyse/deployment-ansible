3eeb77178fd9cb75e08c811049fda86a1fe58e00
Henri Buyse <henri.buyse@gmail.com>
2025-03-13 12:22:41 +0100

    fix(sway): updated service files

--- roles/sway/files/service/sway-wrapper.sh
+++ roles/sway/files/service/sway-wrapper.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# # robustness: if the previous graphical session left some failed units,
+# reset them so that they don't break this startup
+for unit in $(systemctl --user --no-legend --state=failed --plain list-units | cut -f1 -d' '); do
+    partof="$(systemctl --user show -p PartOf --value "$unit")"
+    for target in sway-session.target wayland-session.target graphical-session.target; do
+        if [ "$partof" = "$target" ]; then
+            systemctl --user reset-failed "$unit"
+            break
+        fi
+    done
+done
+
 # Session
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
@@ -9,21 +21,21 @@ set -a
 
 # Import openSUSEway environment variables
 if [ -f /etc/sway/env ]; then
-	. /etc/sway/env
+    . /etc/sway/env
 fi
 
 # Import environment.d variables by calling the systemd generator
 if [ -f /usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator ]; then
-	eval "$(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)"
+    eval "$(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)"
 fi
 set +a
 
 # Set dependencies to run with proprietary drivers
 if grep -qE "nvidia|fglrx" /proc/modules; then
-	export WLR_NO_HARDWARE_CURSORS=1
-	unsupported_gpu="--unsupported-gpu"
+    export WLR_NO_HARDWARE_CURSORS=1
+    unsupported_gpu="--unsupported-gpu"
 else
-	unsupported_gpu=""
+    unsupported_gpu=""
 fi
 
 # Start the Sway session
