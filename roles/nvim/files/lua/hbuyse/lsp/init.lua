d2b113c965b528ae0eb038de0818d1237fc5d54d
Henri Buyse <henri.buyse@gmail.com>
2025-04-11 15:25:24 +0200

    feat(nvim): remove override signature_help and hover

--- roles/nvim/files/lua/hbuyse/lsp/init.lua
+++ roles/nvim/files/lua/hbuyse/lsp/init.lua
@@ -46,27 +46,6 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
-local hover = vim.lsp.buf.hover
----@diagnostic disable-next-line: duplicate-set-field
-vim.lsp.buf.hover = function()
-  return hover({
-    border = vim.g.border_style,
-    max_height = math.floor(vim.o.lines * 0.5),
-    max_width = math.floor(vim.o.columns * 0.4),
-  })
-end
-
-local signature_help = vim.lsp.buf.signature_help
----@diagnostic disable-next-line: duplicate-set-field
-vim.lsp.buf.signature_help = function()
-  return signature_help({
-    border = vim.g.border_style,
-    focusable = false,
-    max_height = math.floor(vim.o.lines * 0.5),
-    max_width = math.floor(vim.o.columns * 0.4),
-  })
-end
-
 --- Setup mapping when attaching LSP server
 ---@param client table LSP client
 ---@param bufnr integer Buffer number
