770b80149212259e69ddd0fce488ff731fbefb34
Henri Buyse <henri.buyse@gmail.com>
2025-07-11 22:29:20 +0200

    fix(nvim): fix cssls config

--- roles/nvim/files/lsp/cssls.lua
+++ roles/nvim/files/lsp/cssls.lua
@@ -2,7 +2,7 @@ return {
   cmd = { 'vscode-css-language-server', '--stdio' },
   filetypes = { 'css', 'scss', 'less' },
   init_options = { provideFormatter = true }, -- needed to enable formatting capabilities
-  root_dir = { 'package.json', '.git' },
+  root_markers = { 'package.json', '.git/' },
   single_file_support = true,
   settings = {
     css = { validate = true },
