f63ef3589627cbf461c0a330086f47931f70c417
Henri Buyse <henri.buyse@gmail.com>
2025-09-06 21:36:11 +0200

    feat(wlsunset): add role and service

--- roles/waybar/files/config/scripts/sunset.sh
+++ roles/waybar/files/config/scripts/sunset.sh
@@ -1,55 +1,23 @@
 #!/usr/bin/env sh
 
-config="$HOME/.config/wlsunset/config"
-
 send_signal() {
     pkill -x -SIGRTMIN+6 'waybar'
 }
 
-#Startup function
-start() {
-    [ -f "$config" ] && . "$config"
-    temp_low=${temp_low:-"4000"}
-    temp_high=${temp_high:-"6500"}
-    duration=${duration:-"900"}
-    sunrise=${sunrise:-"07:00"}
-    sunset=${sunset:-"19:00"}
-    location=${location:-"on"}
-    fallback_longitude=${fallback_longitude:-"8.7"}
-    fallback_latitude=${fallback_latitude:-"50.1"}
-
-    if [ "${location}" = "on" ]; then
-        if [ -z ${longitude+x} ] || [ -z ${latitude+x} ]; then
-            GEO_CONTENT=$(curl -sL http://ip-api.com/json/)
-        fi
-        longitude=${longitude:-$(echo "$GEO_CONTENT" | jq '.lon // empty')}
-        longitude=${longitude:-$fallback_longitude}
-        latitude=${latitude:-$(echo "$GEO_CONTENT" | jq '.lat // empty')}
-        latitude=${latitude:-$fallback_latitude}
-
-        echo longitude: "$longitude" latitude: "$latitude"
-
-        wlsunset -l "$latitude" -L "$longitude" -t "$temp_low" -T "$temp_high" -d "$duration" &
-    else
-        wlsunset -t "$temp_low" -T "$temp_high" -d "$duration" -S "$sunrise" -s "$sunset" &
-    fi
-}
-
-#Accepts managing parameter
 case "$1" in
 'off')
-    pkill -x wlsunset
+    systemctl --user stop wlsunset
     send_signal
     ;;
 'on')
-    start
+    systemctl --user start wlsunset
     send_signal
     ;;
 'toggle')
-    if pkill -x -0 wlsunset; then
-        pkill -x wlsunset
+    if systemctl --user is-active --quiet wlsunset.service; then
+        systemctl --user stop wlsunset.service
     else
-        start
+        systemctl --user start wlsunset.service
     fi
     send_signal
     ;;
@@ -60,12 +28,12 @@ case "$1" in
 esac
 
 #Returns a string for Waybar
-if pkill -x -0 wlsunset; then
+if systemctl --user --quiet is-active wlsunset; then
     class="activated"
-    text="location-based gamma correction"
+    text="Location-based gamma correction"
 else
     class="deactivated"
-    text="no gamma correction"
+    text="No gamma correction"
 fi
 
 printf '{"alt":"%s", "class": "%s", "tooltip":"%s"}\n' "$class" "$class" "$text"
