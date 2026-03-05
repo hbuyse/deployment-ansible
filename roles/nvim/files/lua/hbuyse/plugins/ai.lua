2611450de42cf457f39c41e274da68d876f7e688
Henri Buyse <henri.buyse@gmail.com>
2026-03-05 09:53:37 +0100

    feat(nvim): add avante.nvim plugin for AI support

---
+++ roles/nvim/files/lua/hbuyse/plugins/ai.lua
@@ -0,0 +1,43 @@
+return {
+  'yetone/avante.nvim',
+  enabled = false, -- Disable for now
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- ⚠️ must add this setting! ! !
+  build = 'make',
+  event = 'VeryLazy',
+  version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
+  opts = {
+    -- add any opts here
+    -- this file can contain specific instructions for your project
+    instructions_file = 'AGENTS.md',
+    -- for example
+    auto_suggestions_provider = nil, -- désactive copilotauto_suggestions_provider:q
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'folke/snacks.nvim', -- for input provider dressing
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
