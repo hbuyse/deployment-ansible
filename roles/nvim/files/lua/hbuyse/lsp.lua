62aded91d43cd4e2907c9a0ba1a82c1040f7edbe
Henri Buyse <henri.buyse@gmail.com>
2026-03-05 08:51:46 +0100

    feat(nvim): all lsp configs run the 'on_attach'

--- roles/nvim/files/lua/hbuyse/lsp.lua
+++ roles/nvim/files/lua/hbuyse/lsp.lua
@@ -198,15 +198,8 @@ local function on_attach(client, bufnr)
   end
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    on_attach(client, bufnr)
-  end,
-})
-
 vim.lsp.config('*', {
+  on_attach = on_attach,
   root_markers = {
     '.git',
   },
