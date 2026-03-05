58330cdaab02d69b4d00e6be55f64552e4edecc7
Henri Buyse <henri.buyse@gmail.com>
2026-03-05 09:45:20 +0100

    feat(nvim): do not load treesitter if file greater than 100KB

--- roles/nvim/files/lua/hbuyse/plugins/treesitter.lua
+++ roles/nvim/files/lua/hbuyse/plugins/treesitter.lua
@@ -84,12 +84,16 @@ return {
       highlight = {
         enable = true, -- false will disable the whole extension
         -- Disable slow treesitter highlight for large files
-        disable = function(_, buf)
+        is_supported = function(_, buf)
           local max_filesize = 100 * 1024 -- 100 KB
+
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
-            return true
+            vim.notify('Treesitter disabled')
+            return false
           end
+
+          return true
         end,
       },
       indent = {
